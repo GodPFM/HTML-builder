060ac4c35d13dae5a4c854e55fcb204ce68c0b78
GodPFM <asmeev.daniil@yandex.ru>
2022-11-06 21:09:06 +0500

    feat: add task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathOfNewFolder = path.resolve(__dirname,'files-copy');
+const pathOfCopyFolder = path.resolve(__dirname, 'files');
+
+fs.mkdir(pathOfNewFolder,{recursive: true}, (err) => {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
+fs.readdir(pathOfNewFolder, (err, files) => {
+  if (err) {
+    throw new Error(err);
+  }
+  for (let i = 0; i < files.length;i++) {
+    fs.unlink(path.resolve(pathOfNewFolder, files[i]), (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    })
+  }
+})
+
+fs.readdir(pathOfCopyFolder, (err, files) => {
+  if (err) {
+    throw new Error(err);
+  }
+  for (let i = 0; i < files.length;i++) {
+    fs.copyFile(path.resolve(pathOfCopyFolder,files[i]), path.resolve(pathOfNewFolder,files[i]), (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    })
+  }
+})
