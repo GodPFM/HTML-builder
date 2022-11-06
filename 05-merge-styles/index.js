a7accf16c25a92922461a91bb195e0ff00c7aeac
GodPFM <asmeev.daniil@yandex.ru>
2022-11-06 21:09:36 +0500

    feat: add task solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderWithStyles = path.resolve(__dirname,'styles');
+const folderWithProject = path.resolve(__dirname,'project-dist');
+
+const writeStream = fs.createWriteStream(path.resolve(folderWithProject, 'bundle.css'));
+
+fs.readdir(folderWithStyles, {withFileTypes: true}, (err, files) => {
+  for (let i = 0; i < files.length; i++) {
+    if (path.extname(files[i].name) === '.css') {
+      fs.readFile(path.resolve(folderWithStyles, files[i].name),(err, data) => {
+        if (err) {
+          throw new Error(err);
+        } else {
+          writeStream.write(data + '\n');
+        }
+      })
+    }
+  }
+})
