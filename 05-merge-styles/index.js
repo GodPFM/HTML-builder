5f1bf7012e416de9d594afeb2a490cb797e695ba
GodPFM <asmeev.daniil@yandex.ru>
2022-11-07 23:24:03 +0500

    fix: change the way to write into file from write() to pipe()

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -7,16 +7,11 @@ const folderWithProject = path.resolve(__dirname,'project-dist');
 const writeStream = fs.createWriteStream(path.resolve(folderWithProject, 'bundle.css'));
 
 fs.readdir(folderWithStyles, {withFileTypes: true}, (err, files) => {
-  for (let i = 0; i < files.length; i++) {
-    if (path.extname(files[i].name) === '.css') {
-      fs.readFile(path.resolve(folderWithStyles, files[i].name),(err, data) => {
-        if (err) {
-          throw new Error(err);
-        } else {
-          console.log(i);
-          writeStream.write(data + '\n');
-        }
-      })
+  for (let item of files) {
+    if (path.extname(item.name) === '.css') {
+      const pathToFile = path.resolve(folderWithStyles, item.name);
+      let copyOfFile = fs.createReadStream(pathToFile);
+      copyOfFile.pipe(writeStream);
     }
   }
 })
